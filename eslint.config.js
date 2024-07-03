efac60baa735fdbb858525353a17d90db628d18b
daniel <daniel.blindr@gmail.com>
2024-07-03 14:16:05 +0300

    Eslint imports sort and unused imports

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,43 @@
 import eslint from "@eslint/js"
 import tsEslint from "typescript-eslint"
 import prettierConfig from "eslint-config-prettier"
+import unusedImports from "eslint-plugin-unused-imports"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
 
-export default tsEslint.config({
-  extends: [
-    eslint.configs.recommended,
-    ...tsEslint.configs.recommendedTypeChecked,
-    ...tsEslint.configs.stylisticTypeChecked,
-    prettierConfig,
-  ],
-  languageOptions: {
-    parserOptions: {
-      project: true,
-      tsconfigRootDir: import.meta.dirname,
+export default tsEslint.config(
+  {
+    ignores: ["dist/**/*", ".yarn/**/*", "eslint.config.js", ".pnp.*"],
+  },
+  eslint.configs.recommended,
+  ...tsEslint.configs.recommendedTypeChecked,
+  ...tsEslint.configs.stylisticTypeChecked,
+  prettierConfig,
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+      "simple-import-sort": simpleImportSort,
+    },
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "warn",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+
+      "simple-import-sort/imports": "warn",
+      "simple-import-sort/exports": "warn",
     },
   },
-  ignores: ["dist/**/*", ".yarn/**/*", "eslint.config.js", ".pnp.*"],
-})
+)
